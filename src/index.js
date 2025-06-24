bddd3ce432fc57888285383369fc12b1da2f603f
ykabusalah <42948521+ykabusalah@users.noreply.github.com>
2025-06-24 16:08:20 -0500

    added info and thank you pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import App from './App';
+import Info from './Info';
+import ThankYou from './ThankYou';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/info" />} />
+        <Route path="/info" element={<Info />} />
+        <Route path="/draw" element={<App />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
